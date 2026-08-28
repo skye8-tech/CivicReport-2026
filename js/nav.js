b4a30cebb7d47ef499d89febf442fe3156baa96a
nnamkelly1-6449 <nnamkelly1@gmail.com>
2026-08-28 09:53:24 +0100

    add burger button

--- js/nav.js
+++ js/nav.js
@@ -153,4 +153,18 @@ navbar.appendChild(navRight);
 
 document.body.prepend(navbar);
 
+const burgerBtn = document.getElementById("burgerBtn");
+const sidebar = document.querySelector(".sidebar");
 
+burgerBtn.addEventListener("click", function () {
+  sidebar.classList.toggle("open");
+});
+
+// close sider when u click link
+document.querySelectorAll(".sidebar a").forEach(function (link) {
+  link.addEventListener("click", function () {
+    if (window.innerWidth <= 768) {
+      sidebar.classList.remove("open");
+    }
+  });
+});
